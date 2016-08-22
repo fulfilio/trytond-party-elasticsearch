9b5bb916db44ecce3dc939115a7b226203b1a584
Samaksh Jain <samakshjain@live.com>
2016-08-22 16:50:15 +0530

    Migrated to 4.0.0.1
    [#128292069]

--- tests/test_views_depends.py
+++ tests/test_views_depends.py
@@ -5,43 +5,16 @@
     :copyright: (C) 2015 by Openlabs Technologies & Consulting (P) Limited
     :license: BSD, see LICENSE for more details.
 """
-import sys
-import os
-DIR = os.path.abspath(os.path.normpath(os.path.join(
-    __file__, '..', '..', '..', '..', '..', 'trytond'
-)))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
 import unittest
-
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class TestViewsDepends(unittest.TestCase):
+class TestViewsDepends(ModuleTestCase):
     '''
     Test views and depends
     '''
-
-    def setUp(self):
-        """
-        Set up data used in the tests.
-        this method is called before each test function execution.
-        """
-        trytond.tests.test_tryton.install_module('party_elasticsearch')
-
-    @unittest.skip('No Views')
-    def test0005views(self):
-        '''
-        Test views.
-        '''
-        test_view('party_elasticsearch')
-
-    def test0006depends(self):
-        '''
-        Test depends.
-        '''
-        test_depends()
+    module = 'party_elasticsearch'
 
 
 def suite():
